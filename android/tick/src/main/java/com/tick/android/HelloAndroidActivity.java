93c729ed8f11eda244cacbb92eb99ef006dddc3c
Louis-Philippe Lebouthillier <pokstar@gmail.com>
2013-02-13 23:04:00 -0500

    Having fun with ActionBarSherlock
    added maven support for ActionBarSherlock
    added top menu

--- android/tick/src/main/java/com/tick/android/HelloAndroidActivity.java
+++ android/tick/src/main/java/com/tick/android/HelloAndroidActivity.java
@@ -1,25 +1,38 @@
 package com.tick.android;
 
-import com.tick.android.R;
-import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class HelloAndroidActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+
+public class HelloAndroidActivity extends SherlockActivity {
 
     private static String TAG = "tick";
 
     /**
      * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     * 
+     * @param savedInstanceState
+     *            If the activity is being re-initialized after previously being
+     *            shut down then this Bundle contains the data it most recently
+     *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+     *            is null.</b>
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate");
+        Log.i(TAG, "onCreate()");
         setContentView(R.layout.main);
     }
-}
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(TAG, "onCreateOptionsMenu()");
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.main_activity, menu);
+        return true;
+    }
+
+}
